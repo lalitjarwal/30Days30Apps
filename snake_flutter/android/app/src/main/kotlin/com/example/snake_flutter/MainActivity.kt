93304c9c8cc2226f32615aaceca02d23be9f55f5
lalitjarwal <lalitjarwal315@gmail.com>
2021-03-30 12:14:35 +0530

    snake game in flutter

---
+++ snake_flutter/android/app/src/main/kotlin/com/example/snake_flutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.snake_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ snake_flutter/lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'game.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(primarySwatch: Colors.green),
+      title: 'Olden Days Snake Game',
+      home: Home(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Snake Game"),
+        centerTitle: true,
+      ),
+      backgroundColor: Colors.lightGreen[100],
+      body: Game(),
+    );
+  }
+}
