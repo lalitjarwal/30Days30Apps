fe9e63813804c996f4b1131c4e3bd513b501f4a5
lalitjarwal <lalitjarwal315@gmail.com>
2021-04-03 12:53:38 +0530

    init

---
+++ scientific_calculator/android/app/src/main/kotlin/com/example/scientific_calculator/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.scientific_calculator
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ scientific_calculator/lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'scientificCalculator.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Scientific Calculator',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.purple,
+      ),
+      home: ScientificCalculator(),
+    );
+  }
+}
