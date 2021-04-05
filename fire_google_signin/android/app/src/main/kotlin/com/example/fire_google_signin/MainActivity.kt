0ec5c6074ca09de17051d904429f63708bab3ead
lalitjarwal <lalitjarwal315@gmail.com>
2021-04-05 13:50:48 +0530

    init

---
+++ fire_google_signin/android/app/src/main/kotlin/com/example/fire_google_signin/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.fire_google_signin
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ fire_google_signin/lib/home.dart
@@ -0,0 +1,34 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget {
+  const Home({Key key, this.userCred}) : super(key: key);
+  final UserCredential userCred;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Material App Bar'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            CircleAvatar(
+              radius: 60,
+              backgroundImage: NetworkImage('${userCred.user.photoURL}'),
+            ),
+            SizedBox(
+              height: 20.0,
+            ),
+            Text('Name: ${userCred.user.displayName}'),
+            SizedBox(
+              height: 20.0,
+            ),
+            Text('Email: ${userCred.user.email}')
+          ],
+        ),
+      ),
+    );
+  }
+}
