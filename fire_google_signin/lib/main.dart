a5de52c70c171321a34e4fccb54d63b14dc03b15
lalitjarwal <lalitjarwal315@gmail.com>
2021-04-07 12:29:13 +0530

    fix

---
+++ fire_google_signin/lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:fire_google_signin/signin.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Material App',
+      
+      home: GSignIn(),
+    );
+  }
+}
